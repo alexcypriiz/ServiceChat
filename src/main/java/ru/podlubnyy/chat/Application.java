bbd426039374fc960710f67fdc7fd4412fa33844
alexcypriiz <alexey.podlubny@yandex.ru>
2021-08-02 02:31:31 +0400

    feat: Add message chat

---
+++ src/main/java/ru/podlubnyy/chat/Application.java
@@ -0,0 +1,15 @@
+package ru.podlubnyy.chat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created by Alexey Podlubnyy on 01.08.2021
+ */
+
+@SpringBootApplication
+public class Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
